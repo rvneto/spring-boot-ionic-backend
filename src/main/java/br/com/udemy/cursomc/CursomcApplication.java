0255db42aecf54850991e98db283877558e5b440
Roberto de Vargas <rvneto.rs@gmail.com>
2021-03-30 20:51:50 -0300

    Projeto criado

---
+++ src/main/java/br/com/udemy/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
