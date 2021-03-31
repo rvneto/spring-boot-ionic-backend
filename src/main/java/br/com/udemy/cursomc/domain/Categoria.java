3399b8e0dce369d96f18080552730d8b0900ef31
Roberto de Vargas <rvneto.rs@gmail.com>
2021-03-30 21:23:16 -0300

    Testando a primeira classe de domínio - Categoria

---
+++ src/main/java/br/com/udemy/cursomc/domain/Categoria.java
@@ -0,0 +1,50 @@
+package br.com.udemy.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+// Representa a entidade (tabela de um banco de dados por exemplo)
+public class Categoria implements Serializable { // Indica que os objetos dela podem ser convertidos em uma sequencia de bytes
+
+    private static final long serialVersionUID = 1;
+
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/br/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/udemy/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.udemy.cursomc.resources;
 
+import br.com.udemy.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController // Indica que a classe é um controladore REST
 @RequestMapping(value = "/categorias") // Indica que responde por esse endpoint
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET) // Indica que será um método GET no método
-    public String listar() {
-        return "REST está funcionando!";
+    @RequestMapping(method = RequestMethod.GET) // Indica que será um método GET
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+
+        return categorias;
     }
 
 }
