514bba752ab4f8b2bee6432115ac1e47bc22604e
Roberto de Vargas <rvneto.rs@gmail.com>
2021-03-31 16:14:48 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/br/com/udemy/cursomc/CursomcApplication.java
@@ -1,13 +1,16 @@
 package br.com.udemy.cursomc;
 
 import br.com.udemy.cursomc.domain.Categoria;
+import br.com.udemy.cursomc.domain.Produto;
 import br.com.udemy.cursomc.repositories.CategoriaRepository;
+import br.com.udemy.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,6 +19,9 @@ public class CursomcApplication implements CommandLineRunner { // Permite implem
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    @Autowired
+    ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
@@ -23,10 +29,21 @@ public class CursomcApplication implements CommandLineRunner { // Permite implem
     @Override
     public void run(String... args) throws Exception {
 
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Collections.singletonList(p2));
+
+        p1.getCategorias().addAll(Collections.singletonList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Collections.singletonList(cat1));
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
--- src/main/java/br/com/udemy/cursomc/domain/Produto.java
+++ src/main/java/br/com/udemy/cursomc/domain/Produto.java
@@ -6,9 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-// Representa a entidade (tabela de um banco de dados por exemplo)
 @Entity
-public class Categoria implements Serializable { // Indica que os objetos dela podem ser convertidos em uma sequencia de bytes
+public class Produto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -18,15 +17,21 @@ public class Categoria implements Serializable { // Indica que os objetos dela p
 
     private String nome;
 
-    @ManyToMany(mappedBy = "categorias")
-    private List<Produto> produtos = new ArrayList<>();
+    private Double preco;
 
-    public Categoria() {
+    @ManyToMany
+    @JoinTable(name = "PRODUTO_CATEGORIA",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public Produto() {
     }
 
-    public Categoria(Integer id, String nome) {
+    public Produto(Integer id, String nome, Double preco) {
         this.id = id;
         this.nome = nome;
+        this.preco = preco;
     }
 
     public Integer getId() {
@@ -45,20 +50,28 @@ public class Categoria implements Serializable { // Indica que os objetos dela p
         this.nome = nome;
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
+    public Double getPreco() {
+        return preco;
     }
 
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Categoria categoria = (Categoria) o;
-        return id.equals(categoria.id);
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
     }
 
     @Override
