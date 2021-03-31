faeb7ca716e7b73202ef92703ef6ded91472e573
Roberto de Vargas <rvneto.rs@gmail.com>
2021-03-30 22:07:03 -0300

    Criando repository e service para Categoria

--- src/main/java/br/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/udemy/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,25 @@
 package br.com.udemy.cursomc.resources;
 
 import br.com.udemy.cursomc.domain.Categoria;
+import br.com.udemy.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController // Indica que a classe é um controladore REST
 @RequestMapping(value = "/categorias") // Indica que responde por esse endpoint
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET) // Indica que será um método GET
-    public List<Categoria> listar() {
+    @Autowired
+    private CategoriaService categoriaService;
 
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
-
-        List<Categoria> categorias = new ArrayList<>();
-        categorias.add(cat1);
-        categorias.add(cat2);
-
-        return categorias;
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET) // Indica que será um método GET
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria categoria = categoriaService.buscar(id);
+        return ResponseEntity.ok().body(categoria);
     }
 
 }
---
+++ src/main/java/br/com/udemy/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package br.com.udemy.cursomc.services;
+
+import br.com.udemy.cursomc.domain.Categoria;
+import br.com.udemy.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired // Instancia o objeto (Spring)
+    private CategoriaRepository categoriaRepository; // Dependencia (injeção do spring)
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id); // busca a categoria pelo id
+        return categoria.orElse(null);
+    }
+
+}
