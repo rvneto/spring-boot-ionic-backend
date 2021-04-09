c338d2a2c2c6c4712170b887055575b7933d7e67
Roberto de Vargas <rvneto.rs@gmail.com>
2021-04-09 14:22:46 -0300

    Personalizando o retorno da validação

--- src/main/java/br/com/udemy/ionicbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/udemy/ionicbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -2,8 +2,11 @@ package br.com.udemy.ionicbackend.resources.exceptions;
 
 import br.com.udemy.ionicbackend.services.exceptions.DataIntegrityException;
 import br.com.udemy.ionicbackend.services.exceptions.ObjectNotFoundException;
+import br.com.udemy.ionicbackend.services.exceptions.ValidationError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,6 +24,15 @@ public class ResourceExceptionHandler { // interceptador de exceções
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
         StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação!", System.currentTimeMillis());
+        for (FieldError erro : e.getBindingResult().getFieldErrors()) {
+            error.addError(erro.getField(), erro.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
