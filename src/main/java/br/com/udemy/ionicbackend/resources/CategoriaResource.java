872ae106b27df69bf52663d4ed0deccf01f7dc0a
Roberto de Vargas <rvneto.rs@gmail.com>
2021-04-08 21:32:03 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/br/com/udemy/ionicbackend/resources/CategoriaResource.java
+++ src/main/java/br/com/udemy/ionicbackend/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import br.com.udemy.ionicbackend.domain.Categoria;
 import br.com.udemy.ionicbackend.dto.CategoriaDTO;
 import br.com.udemy.ionicbackend.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -50,7 +51,20 @@ public class CategoriaResource {
     public ResponseEntity<List<CategoriaDTO>> findAll() {
         List<Categoria> categorias = categoriaService.findAll();
         List<CategoriaDTO> categoriasDTO = categorias.stream().map(
-                categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+                CategoriaDTO::new).collect(Collectors.toList());
+        // categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(categoriasDTO);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(name = "direction", defaultValue = "ASC") String direction) {
+        Page<Categoria> categorias = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> categoriasDTO = categorias.map(CategoriaDTO::new);
+        // categoria -> new CategoriaDTO(categoria));
         return ResponseEntity.ok().body(categoriasDTO);
     }
 }
--- src/main/java/br/com/udemy/ionicbackend/services/CategoriaService.java
+++ src/main/java/br/com/udemy/ionicbackend/services/CategoriaService.java
@@ -6,6 +6,9 @@ import br.com.udemy.ionicbackend.services.exceptions.DataIntegrityException;
 import br.com.udemy.ionicbackend.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -44,5 +47,10 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return categoriaRepository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
