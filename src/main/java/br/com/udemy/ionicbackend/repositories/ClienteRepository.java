295366f0e5b09c77e2912f510cfbeea817812d55
Roberto de Vargas <rvneto.rs@gmail.com>
2021-04-09 16:24:20 -0300

    Validação customizada: e-mail não repetido na inserção de Cliente

--- src/main/java/br/com/udemy/ionicbackend/repositories/ClienteRepository.java
+++ src/main/java/br/com/udemy/ionicbackend/repositories/ClienteRepository.java
@@ -3,7 +3,13 @@ package br.com.udemy.ionicbackend.repositories;
 import br.com.udemy.ionicbackend.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-@Repository // Objeto de acesso a dados
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    // não necessita ser envolvida como uma transação de BD / performance, não locka
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
+
 }
--- src/main/java/br/com/udemy/ionicbackend/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/udemy/ionicbackend/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package br.com.udemy.ionicbackend.services.validation;
 
+import br.com.udemy.ionicbackend.domain.Cliente;
 import br.com.udemy.ionicbackend.domain.enums.TipoCliente;
 import br.com.udemy.ionicbackend.dto.ClienteNewDTO;
+import br.com.udemy.ionicbackend.repositories.ClienteRepository;
 import br.com.udemy.ionicbackend.services.exceptions.FieldMessage;
 import br.com.udemy.ionicbackend.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +14,9 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -30,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if (dto.getTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) &&
                 !BR.isValidCnpj(dto.getCpfOuCnpj())) {
             erros.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+        }
+
+        Cliente cliente = clienteRepository.findByEmail(dto.getEmail());
+        if (cliente != null) {
+            erros.add(new FieldMessage("Email", "E-mail já existente!"));
         }
 
         for (FieldMessage e : erros) { // precisa fazer isso pra transformar a lista de erros personalizados de forma que o framework consiga entender
