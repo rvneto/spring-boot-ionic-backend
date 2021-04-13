cb10d4efc6209c325cdf4ef04b603c20f98b009b
Roberto de Vargas <rvneto.rs@gmail.com>
2021-04-13 08:49:20 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/udemy/ionicbackend/config/DevConfig.java
@@ -0,0 +1,29 @@
+package br.com.udemy.ionicbackend.config;
+
+import br.com.udemy.ionicbackend.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy)) {
+            dbService.instantiateDatabase();
+        }
+        return true;
+    }
+}
